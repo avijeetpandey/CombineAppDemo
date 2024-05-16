b70a0fd62bee19ade6928525ae773229cf55c035
avijeet <avijeetpandey25@gmail.com>
2024-05-17 00:38:48 +0530

    Initial Commit

---
+++ CombineAppDemo/CombineAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineAppDemoApp.swift
+//  CombineAppDemo
+//
+//  Created by Avijeet on 17/05/24.
+//
+
+import SwiftUI
+
+@main
+struct CombineAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
